b491fca88fd6ed9c9a874d4e303627ed81a3dbcf
Chris Knott <s206362@uos.ac.uk>
2020-10-19 10:49:46 +0100

    After activity 10
    This didn't work

---
+++ Gruntfile.js
@@ -0,0 +1,32 @@
+module.exports = function (grunt) {
+    const sass = require('node-sass');
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        sass: {
+            main: {
+                options: {
+                    sourceMap: true,
+                    outputStyle: 'compressed',
+                    implementation: sass,
+                },
+                files: {
+                    './css/styles.css': './scss/main.scss'
+                }
+            },
+        },
+        watch: {
+            scss: {
+                files: ['./scss/**/*.scss'],
+                tasks: ['sass:main'],
+                options: {
+                    spawn: false,
+                },
+            },
+        },
+
+    });
+
+    grunt.loadNpmTasks('grunt-sass');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.registerTask('default', ['watch:scss']);
+};
