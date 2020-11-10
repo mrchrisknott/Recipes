e767c28ad44d818311f1d92dd2f3040aff2d90d8
Chris Knott <s206362@uos.ac.uk>
2020-11-10 16:55:11 +0000

    The end

--- Gruntfile.js
+++ Gruntfile.js
@@ -23,8 +23,61 @@ module.exports = function (grunt) {
                 },
             },
         },
+        // Start - Did I position the following correctly for Activity 15 / Step 7?
+        watch: {
+            js: {
+                files: ['./scripts/**/*.js'],
+                tasks: ['uglify:main'],
+                options: {
+                    spawn: false,
+                },
+            },
+        },
+        //   End - Did I position the above correctly for Activity 15 / Step 7?
+
+        uglify: {
+            main: {
+                options: {
+                    sourceMap: false,
+                    compress: true,
+                    mangle: false,
+                },
+                files: {
+                    "./js/scripts.min.js": ["./scripts/**/*.js"],
+                },
+            },
+            vendor: {
+                options: {
+                    sourceMap: false,
+                    compress: true,
+                    mangle: false,
+                },
+                files: {
+                    "./js/scripts-vendor.min.js": [
+                        "./node_modules/jquery/dist/jquery.min.js",
+                        "./node_modules/bootstrap/dist/js/bootstrap.min.js",
+                        "./node_modules/@glidejs/glide/dist/glide.min.js",
+                        "./node_modules/lightbox2/dist/js/lightbox.min.js",
+                    ],
+                },
+            },
+        },
+        concurrent: {
+            options: {
+                logConcurrentOutput: true,
+                limit: 10,
+            },
+            watchall: {
+                tasks: ["watch:scss", "watch:js"],
+            },
+        },
+
+
     });
     grunt.loadNpmTasks('grunt-sass');
     grunt.loadNpmTasks('grunt-contrib-watch');
-    grunt.registerTask('default', ['watch:scss']);
+    grunt.registerTask('default', ['concurrent:watchall']);
+    grunt.registerTask("vendors", ["uglify:vendor"]);
+    grunt.loadNpmTasks("grunt-concurrent");
+    grunt.loadNpmTasks("grunt-contrib-uglify");
 };
